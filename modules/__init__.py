5a20134e5f08294891a56ad14607c4acb3513322
TheRagingRyan <rbero123@gmail.com>
2023-10-24 22:06:27 -0400

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,13 +1,12 @@
 import subprocess
 import os
 import ast
-import configparser
 from pathlib import Path
 
 ### Check for modules on run time
 def module_check() -> bool:
     modules_to_check = ['dearpygui', 'pyautogui', 'easygui', 'bs4', 'requests', 'PIL', 'pypresence', 'icecream', 'urllib3']
-    modules_to_install = ['dearpygui', 'pyautogui', 'easygui', 'beautifulsoup4', 'requests', 'Pillow', 'pypresence', 'icecream', 'urllib=1.26.7']
+    modules_to_install = ['dearpygui', 'pyautogui', 'easygui', 'beautifulsoup4', 'requests', 'Pillow', 'pypresence', 'icecream', 'urllib3==1.26.7']
     
     for i in range(len(modules_to_check)):
         try:
@@ -21,11 +20,10 @@ module_check()
 #########################################################################################################################################################  
 from . functions import Config_AIO
 from . classes import Leader_Skill_Info
+from . configs import Config_Read
 
 Config_Path = str(Path.home()) + '/Unit Creation Tool' + '/config.ini'
-config = configparser.ConfigParser()
-config.read(Config_Path)
-
+config = Config_Read()
 
 ### Necessary for program to run properly. 
 ### Asks for decrypted database location, as well as grab categories from the Dokkan.wiki and save them to the config.
